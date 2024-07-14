33d4846a7018af4615693a69a3b3aaeeb6ee013e
AhmedAbdel-Aal <35325857+AhmedAbdel-Aal@users.noreply.github.com>
2024-07-14 20:07:02 +0200

    restructure

---
+++ TND/zero-shot/zero-shot.py
@@ -0,0 +1,12 @@
+import ollama
+
+
+
+
+response = ollama.chat(model='mistral:7b', messages=[
+ {
+    'role': 'system',
+    'content': f'<s>[INST] {prompt} [/INST]'
+  },
+])
+print(response['message']['content'])
